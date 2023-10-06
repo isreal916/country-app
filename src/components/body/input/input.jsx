7d6fa39014659ff6ccf9ab746b5fbaf6fef466db
isreal916 <isrealadeoluwa89@gmail.com>
2023-10-06 23:55:18 +0100

    commit

--- src/components/body/input/input.jsx
+++ src/components/body/input/input.jsx
@@ -7,13 +7,13 @@ import { backgroundContext } from '../../../App';
 
   return(
     <div className="input-container" >
-      <div className="input-container-text" style={(isDarkmode)?{backgroundColor:'hsl(209, 23%, 22%)',color:'white'}:{backgroundColor:'white',color:'black', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
+      <div className="input-container-text" style={(isDarkmode)?{backgroundColor:'hsl(209, 23%, 22%)',color:'white'}:{backgroundColor:'white',color:'black', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}} >
         <span><FaSearch/></span>
         
-        <input type="text" name="text" id=""  onChange={(ev)=>handleInput(ev)}  />
+        <input type="text" name="text" id=""  onChange={(ev)=>handleInput(ev)}   />
       </div>
-      <div className="input-container-select"style={(isDarkmode)?{backgroundColor:'hsl(209, 23%, 22%)',color:'white'}:{backgroundColor:'white',color:'black', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
-        <select name="select" id="" onChange={(ev)=>handleInput(ev)}>
+      <div className="input-container-select" >
+        <select name="select" id="" onChange={(ev)=>handleInput(ev)} style={(isDarkmode)?{backgroundColor:'hsl(209, 23%, 22%)',color:'white'}:{backgroundColor:'white',color:'black', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
           <option value="africa">Africa</option>
           <option value="america">America</option>
           <option value="asia">Asia</option>
